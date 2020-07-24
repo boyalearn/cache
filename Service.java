5bdc6d7356628ba8e361a67630d828d4de28c378
zouhuixing <2114517411@qq.com>
2020-07-24 17:33:13 +0800

    Create Service.java

---
+++ Service.java
@@ -0,0 +1,18 @@
+package com.test.service;
+
+import com.smart.cache.annotation.Cache;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Service {
+    @Cache(interval = 20000,expired = 40*1000)
+    public String getContext(String context){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "my context";
+    }
+}
