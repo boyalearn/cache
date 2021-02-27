1c9f5ff59f312bcc5af017a688381a069f867fd0
zouhuixing <2114517411@qq.com>
2021-02-27 20:08:48 +0800

    删除原来的不合理的设计方式。

--- cache-core/src/main/java/cache/invoker/Invoker.java
+++
@@ -1,7 +0,0 @@
-package cache.invoker;
-
-import cache.entity.CallMethod;
-
-public interface Invoker {
-    Object invoker(CallMethod callMethod, Object[] args, Object annotation) throws Throwable;
-}
--- cache-core/src/main/java/com/cache/core/crontab/CronTaskManager.java
+++ cache-core/src/main/java/com/cache/core/crontab/CronTaskManager.java
@@ -1,4 +1,16 @@
 package com.cache.core.crontab;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CronTaskManager {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CacheScheduler.class);
+
+    private final static MethodInvokerQueue QUEUE = new MethodInvokerQueue();
+
+    private static final ExecutorService THREAD_POOL = Executors.newFixedThreadPool(3);
 }
